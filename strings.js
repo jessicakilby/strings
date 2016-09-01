b8586534277bb9b95d613ea04c0ab4725f6872fb
jessica kilby <jessicakilby@gmail.com>
2016-08-31 20:16:41 -0500

    shorter code

--- strings.js
+++ strings.js
@@ -13,20 +13,15 @@ var input = document.getElementById('input');
 var rev = document.getElementById('rev');
 var alph = document.getElementById('alph');
 var pal = document.getElementById('pal');
-// var output = document.getElementById("output");
+var output = document.getElementById("output");
 var clearButton = document.getElementById("clear");
-
-function print(message){
-  var outText = document.getElementById("output");
-  outText.innerHTML = message;
-}
+var outText = document.getElementById("output");
 
 function reversal() {
 	var newInput = input.value;
 	var revInput = newInput.split("").reverse().join("");
 	console.log(revInput);
-	html = "<div>Your sentence in reverse: "+"<strong>"+revInput+"</strong></div>";
-	print(html);
+	outText.innerHTML += "<div>Your sentence in reverse: "+"<strong>"+revInput+"</strong></div>";
 }
 
 function alphabits() {
@@ -34,8 +29,7 @@ function alphabits() {
 	var alph = newInput.split("");
 	var sortAlph = alph.sort().join("");
 	console.log(sortAlph);
-	html = "<div>The characters alphabeticized: "+"<strong>"+sortAlph+"</strong></div>";
-	print(html);
+	outText.innerHTML += "<div>The characters alphabeticized: "+"<strong>"+sortAlph+"</strong></div>";
 }
 
 // Implement the logic in the palindrome function that determine whether the string is a palindrome. If it is, display the text "Your string is a palidrome" in the DOM, below the text input.
@@ -45,11 +39,10 @@ function palindrome() {
 	var palSplit = newInput.split("");
 	var palRev = palSplit.reverse().join("");
 	if (newInput === palRev){
-		html = "<div>Your sentence was a plalindrome: </br>'"+newInput+"' <strong>equals</strong> '"+palRev+"'</div>";
+		html += "<div>Your sentence was a plalindrome: </br>'"+newInput+"' <strong>equals</strong> '"+palRev+"'</div>";
 		print(html);
 	} else {
-		html = "<div>Sorry it is not a plalindrome: </br>'"+newInput+"' <strong>does not equal</strong> '"+palRev+"'</div>";
-		print(html);
+		outText.innerHTML += "<div>Sorry it is not a plalindrome: </br>'"+newInput+"' <strong>does not equal</strong> '"+palRev+"'</div>";
 	}
 }
 
